3c0e42c96b35087e96e70507741aac9a7fba4f66
joseubillus <jose_ubillus@hotmail.com>
2021-09-17 19:50:43 -0500

    Segundo commit

--- app/src/main/java/com/example/apkecomer/MainActivity.java
+++ app/src/main/java/com/example/apkecomer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.apkecomer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 
 import com.example.util.PreCarga;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private TextView Lblpor;
     private ImageView Imgpre;
     private PreCarga pre;
@@ -21,11 +22,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //showSystemUI();
         Lblpor = (TextView) findViewById(R.id.FrmPre_Lblpor);
         Imgpre = (ImageView) findViewById(R.id.FrmPre_ImgPre);
 
         pre = new PreCarga(this,Lblpor);
         pre.execute();
 
+    }
+
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
